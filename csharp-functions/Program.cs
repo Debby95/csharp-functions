40691c122f29521ed8637cb488942081e6d2341c
Debby95 <debbyjacksonstar@hotmail.it>
2022-10-28 16:25:29 +0200

    snack stampa array

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//void StampaArray(int[] array): che preso un array di numeri interi,
+//stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“.
+//Potete prendere quella fatta in classe questa mattina
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+void StampaArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+
+
+    
